bed72cf1a2e5f1c0f2fee69b4ba2c3afdc2afeb1
sebastiangaleano.dev <sebastiangaleano.dev@gmail.com>
2024-10-12 13:12:02 +0000

    class Task created

--- Javascript/Ej_2_JS/index.js
+++ Javascript/Ej_2_JS/index.js
@@ -1,8 +1,8 @@
 /* Gestor de tareas con prioridades
 
 Pautas:
-- Crea una clase Task con propiedades como tÃ­tulo, descripciÃ³n, fecha de vencimiento y prioridad.
-- Implementa mÃ©todos para agregar, eliminar y ordenar tareas.
+- Crea una clase Task con propiedades como título, descripción, fecha de vencimiento y prioridad.✅
+- Implementa métodos para agregar, eliminar y ordenar tareas.
 - Utiliza sort para ordenar las tareas por prioridad.
 
 Datos iniciales:
@@ -10,7 +10,16 @@ Datos iniciales:
 javascript
 const initialTasks = [
   { id: 1, title: "Completar informe", description: "Finalizar informe trimestral", dueDate: "2024-03-15", priority: 2 },
-  { id: 2, title: "ReuniÃ³n con cliente", description: "Presentar propuesta de proyecto", dueDate: "2024-03-10", priority: 1 },
-  { id: 3, title: "Actualizar software", description: "Instalar Ãºltimas actualizaciones", dueDate: "2024-03-20", priority: 3 }
+  { id: 2, title: "Reunión con cliente", description: "Presentar propuesta de proyecto", dueDate: "2024-03-10", priority: 1 },
+  { id: 3, title: "Actualizar software", description: "Instalar últimas actualizaciones", dueDate: "2024-03-20", priority: 3 }
 ];
 */
+
+class Task{
+    constructor(title,description,  dueDate, priority){
+        this.title = title,
+        this.description = description,
+        this.dueDate = dueDate,
+        this.priority = priority
+    }
+}
