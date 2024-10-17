8a0042d0f86ddffa8fe16dc465eec9ba3966b2ca
sebastiangaleano.dev <sebastiangaleano.dev@gmail.com>
2024-10-17 13:17:39 +0000

    Delete cmments

--- Typescript/Clase 4/Ej_3/src/index.js
+++ Typescript/Clase 4/Ej_3/src/index.js
@@ -1,20 +1,4 @@
 "use strict";
-/* Sistema de Logging con Niveles Personalizables
-
-*Enunciado:*
-Desarrolla un sistema de logging que permita registrar mensajes en diferentes niveles
-(DEBUG, INFO, WARN, ERROR).
-El sistema debe permitir la configuración de un nivel mínimo de log y el formato de salida.
-También debe ser flexible para soportar múltiples destinos, como la consola o un archivo.
-
-*Requisitos adicionales:*
-- Implementa un método para configurar el formato de los mensajes de log (e.g., "[FECHA] [NIVEL]: Mensaje").
-- Permite que el sistema de logging filtre los mensajes que están por debajo del nivel mínimo configurado.
-- Implementa un método para cambiar dinámicamente el destino de los logs.
-
-*Código base:*
-typescript
-*/
 class Logger {
     constructor(config) {
         this.levelGrade = {
